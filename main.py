48d3886bdbd67e40f6174387163f0f4fa43edf8c
Mateus Vitorino Guimaraes <mateusvitorino@gmail.com>
2024-12-10 17:58:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import validate_docbr
 
 app = Flask(__name__)
 
@@ -13,6 +14,11 @@ def validar_cep(cep):
         return not data.get("erro", False)
     return False
 
+# Função para validar CNPJ
+def validar_cnpj(cnpj):
+    cnpj_validator = validate_docbr.CNPJ()
+    return cnpj_validator.validate(cnpj)
+
 @app.route('/validate', methods=['GET'])
 def validate_custom_attribute():
     # Obtém os parâmetros da URL
@@ -24,16 +30,23 @@ def validate_custom_attribute():
     if not attribute_type or not key or not value:
         return jsonify({"error": "Missing required parameters"}), 400
 
-    # Valida o formato do CEP
-    if len(value) != 8 or not value.isdigit():
-        return jsonify({"error": "Invalid CEP format"}), 400
-
-    # Valida o CEP com a API externa
-    is_valid = validar_cep(value)
-    if is_valid:
-        return jsonify({"message": "Valid CEP"}), 200
+    # Valida o formato do CEP ou CNPJ
+    if len(value) == 8 and value.isdigit():  # Caso seja um CEP
+        # Valida o CEP com a API externa
+        is_valid = validar_cep(value)
+        if is_valid:
+            return jsonify({"message": "Valid CEP"}), 200
+        else:
+            return jsonify({"error": "Invalid CEP"}), 400
+    elif len(value) == 14 and value.isdigit():  # Caso seja um CNPJ
+        # Valida o CNPJ
+        is_valid = validar_cnpj(value)
+        if is_valid:
+            return jsonify({"message": "Valid CNPJ"}), 200
+        else:
+            return jsonify({"error": "Invalid CNPJ"}), 400
     else:
-        return jsonify({"error": "Invalid CEP"}), 400
+        return jsonify({"error": "Invalid value format"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
